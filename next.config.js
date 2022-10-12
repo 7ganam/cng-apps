82b81d197fabf57c74606db14257e2e037cf3a95
ganam <7ganam@gmail.com>
2022-10-12 08:22:25 +0200

    allowed production errors

--- next.config.js
+++ next.config.js
@@ -12,6 +12,13 @@ const withTM = require('next-transpile-modules')([
 ])
 
 module.exports = withTM({
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true
+  },
   trailingSlash: true,
   reactStrictMode: false,
   experimental: {
